0d61b16f5462055b82efa2d52be34815caeb3951
DartPower <>
2020-11-07 00:08:39 +0300

    Update CommandWarps.java

--- src/main/java/io/dpteam/GUIWarps/commands/CommandWarps.java
+++ src/main/java/io/dpteam/GUIWarps/commands/CommandWarps.java
@@ -37,7 +37,7 @@ public class CommandWarps implements CommandExecutor {
 			int maxItems = 54;
 			int limit = 0;
 			PageInv pageI = new PageInv(page);
-			String warp = (int)iter.next();
+			String Integer.toString(warp) = (int)iter.next();
 			if (page == 1 && Main.ess.getWarps().getCount() > 54) {
 				pageI.getInventory().setItem(53, this.getItemStackWithName(this.config.getString("nextPage"), Material.getMaterial(Integer.toString(this.config.getInt("GUIWarps.nextID")))));
 			} else if (page != 1 && Main.ess.getWarps().getCount() > 53) {
